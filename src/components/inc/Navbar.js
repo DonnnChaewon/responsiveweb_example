d1fef7cd1ec886100bc4d076a594d8a0b8090509
DonnnChaewon <brandonlolz2003.com>
2025-05-19 13:54:27 +0700

    Changed class into className.

--- src/components/inc/Navbar.js
+++ src/components/inc/Navbar.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow">
-            <div class="container-fluid">
-                <Link class="navbar-brand" to="/">TopicNation</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">TopicNation</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link active" to="/">Home</Link>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/topics">Topics</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/topics">Topics</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/about">About Us</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About Us</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/contact">Contact Us</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact Us</Link>
                         </li>
                     </ul>
                 </div>
