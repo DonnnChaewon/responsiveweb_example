5d6f1920e227af19c187c1b84f97f34ab7d06468
DonnnChaewon <brandonlolz2003.com>
2025-05-22 10:45:15 +0700

    Changed the text color of h6.

--- src/components/inc/Footer.js
+++ src/components/inc/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-md-4'>
-                        <h6>Site Information</h6>
+                        <h6 className='text-2xl text-blue-600'>Site Information</h6>
                         <hr />
                         <p className='footer-text'>This is an example of a responsive website about various topics as long they are relevant to readers. The topics are various and of course, no topic is absurd and cringe. The topics here can be random and can be from anywhere and anytime.</p>
                     </div>
                     <div className='col-md-4'>
-                        <h6>Quick Links</h6>
+                        <h6 className='text-2xl text-blue-600'>Quick Links</h6>
                         <hr />
                         <div className='footer-text'><Link to='/'>Home</Link></div>
                         <div className='footer-text'><Link to='/about'>About</Link></div>
@@ -20,7 +20,7 @@ const Footer = () => {
                         <div className='footer-text'><Link to='/topics'>Topics</Link></div>
                     </div>
                     <div className='col-md-4'>
-                        <h6>Contact Information</h6>
+                        <h6 className='text-2xl text-blue-600'>Contact Information</h6>
                         <hr />
                         <div><p className='footer-text mb-1'>San Fransisco, California, USA</p></div>
                         <div><p className='footer-text mb-1'>+1 1234567890</p></div>
